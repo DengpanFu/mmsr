6a874f59dfbc9590929395ee3ef5053acac2aedb
DengpanFu <fdpan@mail.ustc.edu.cn>
2019-12-24 20:58:54 +0800

    Meta

--- codes/data/REDS_dataset.py
+++ codes/data/REDS_dataset.py
@@ -3,13 +3,14 @@ REDS dataset
 support reading images from lmdb, image folder and memcached
 '''
 import os.path as osp
-import random
+import os, random
 import pickle
 import logging
 import numpy as np
 import cv2
 import lmdb
 import torch
+from PIL import Image
 import torch.utils.data as data
 import data.util as util
 try:
@@ -381,7 +382,7 @@ class MetaREDSDatasetV0(data.Dataset):
     def __len__(self):
         return len(self.paths_GT)
 
-class MetaREDSDataset(data.Dataset):
+class MetaREDSDatasetV1(data.Dataset):
     '''
     Reading the training REDS dataset
     key example: 000_00000000
@@ -391,7 +392,7 @@ class MetaREDSDataset(data.Dataset):
     '''
 
     def __init__(self, opt):
-        super(MetaREDSDataset, self).__init__()
+        super(MetaREDSDatasetV1, self).__init__()
         self.opt = opt
         # temporal augmentation
         self.interval_list = opt['interval_list']
@@ -524,6 +525,139 @@ class MetaREDSDataset(data.Dataset):
         img_GT = torch.from_numpy(np.ascontiguousarray(np.transpose(img_GT, (2, 0, 1)))).float()
         img_LQs = torch.from_numpy(np.ascontiguousarray(np.transpose(img_LQs,
                                                                      (0, 3, 1, 2)))).float()
+        # import matplotlib.pyplot as plt
+        # plt.imshow(img_GT.numpy().transpose((1,2,0)))
+        # plt.figure(2)
+        # plt.imshow(img_LQs[0].numpy().transpose((1,2,0))) 
+        # plt.show()
+        return {'LQs': img_LQs, 'GT': img_GT, 'key': key, 'scale': scale}
+
+    def __len__(self):
+        return len(self.paths_GT)
+
+class MetaREDSDataset(data.Dataset):
+    '''
+    Reading the training REDS dataset
+    key example: 000_00000000
+    GT: Ground-Truth;
+    LQ: Low-Quality, e.g., low-resolution/blurry/noisy/compressed frames
+    support reading N LQ frames, N = 1, 3, 5, 7
+    '''
+
+    def __init__(self, opt):
+        super(MetaREDSDataset, self).__init__()
+        self.opt = opt
+        # temporal augmentation
+        self.interval_list = opt['interval_list']
+        self.random_reverse = opt['random_reverse']
+        logger.info('Temporal augmentation interval list: [{}], with random reverse is {}.'.format(
+            ','.join(str(x) for x in opt['interval_list']), self.random_reverse))
+
+        self.half_N_frames = opt['N_frames'] // 2
+        self.GT_root = opt['dataroot_GT']
+        self.LQ_root = opt['dataroot_LQ']
+        self.scales = opt['scale']
+        assert(len(self.scales) > 1)
+
+        seqs = sorted(os.listdir(self.GT_root))
+        self.paths_GT = []
+        for seq in seqs:
+            if not seq in ['000', '011', '015', '020']:
+                names = os.listdir(osp.join(self.GT_root, seq))
+                self.paths_GT.extend([seq + '_' + x[:-4] for x in names])
+
+        assert self.paths_GT, 'Error: GT path is empty.'
+
+    def get_neighbor_list(self, center_frame_idx):
+        #### determine the neighbor frames
+        interval = random.choice(self.interval_list)
+        if self.opt['border_mode']:
+            direction = 1  # 1: forward; 0: backward
+            N_frames = self.opt['N_frames']
+            if self.random_reverse and random.random() < 0.5:
+                direction = random.choice([0, 1])
+            if center_frame_idx + interval * (N_frames - 1) > 99:
+                direction = 0
+            elif center_frame_idx - interval * (N_frames - 1) < 0:
+                direction = 1
+            # get the neighbor list
+            if direction == 1:
+                neighbor_list = list(
+                    range(center_frame_idx, center_frame_idx + interval * N_frames, interval))
+            else:
+                neighbor_list = list(
+                    range(center_frame_idx, center_frame_idx - interval * N_frames, -interval))
+            name_b = '{:08d}'.format(neighbor_list[0])
+        else:
+            # ensure not exceeding the borders
+            while (center_frame_idx + self.half_N_frames * interval >
+                   99) or (center_frame_idx - self.half_N_frames * interval < 0):
+                center_frame_idx = random.randint(0, 99)
+            # get the neighbor list
+            neighbor_list = list(
+                range(center_frame_idx - self.half_N_frames * interval,
+                      center_frame_idx + self.half_N_frames * interval + 1, interval))
+            if self.random_reverse and random.random() < 0.5:
+                neighbor_list.reverse()
+            name_b = '{:08d}'.format(neighbor_list[self.half_N_frames])
+
+        assert len(
+            neighbor_list) == self.opt['N_frames'], 'Wrong length of neighbor list: {}'.format(
+                len(neighbor_list))
+        return neighbor_list
+
+    def read_imgs(self, root_dir, name_a, name_b, scale=None):
+        if scale is None:
+            paths = [osp.join(root_dir, name_a, name_b + '.png')]
+        else:
+            if not isinstance(name_b, (tuple, list)):
+                name_b = [name_b]
+            paths = []
+            for name in name_b:
+                if not isinstance(name, str):
+                    name = "{:08d}".format(name)
+                paths.append(osp.join(root_dir, 'X{:.02f}'.format(scale), 
+                    name_a, name+'.png'))
+        imgs = []
+        for path in paths:
+            imgs.append(np.array(Image.open(path))/255.)
+        return imgs
+
+    def __getitem__(self, index):
+        image_index, scale_index = index
+        scale = self.scales[scale_index]
+        LQ_size = self.opt['LQ_size']
+        key = self.paths_GT[image_index]
+        name_a, name_b = key.split('_')
+        center_frame_idx = int(name_b)
+
+        neighbor_list = self.get_neighbor_list(center_frame_idx)
+
+        #### get the images
+        img_GT = self.read_imgs(self.GT_root, name_a, name_b)[0]
+        img_LQs = self.read_imgs(self.LQ_root, name_a, neighbor_list, scale)
+
+        if self.opt['phase'] == 'train':
+            H, W, _ = img_LQs[0].shape
+            rnd_h = random.randint(0, max(0, H - LQ_size))
+            rnd_w = random.randint(0, max(0, W - LQ_size))
+            img_LQs = [v[rnd_h:rnd_h + LQ_size, rnd_w:rnd_w + LQ_size, :] for v in img_LQs]
+            rnd_h_HR, rnd_w_HR = int(rnd_h * scale), int(rnd_w * scale)
+            GT_size = int(LQ_size * scale)
+            img_GT = img_GT[rnd_h_HR:rnd_h_HR + GT_size, rnd_w_HR:rnd_w_HR + GT_size, :]
+            # augmentation - flip, rotate
+            img_LQs.append(img_GT)
+            rlt = util.augment(img_LQs, self.opt['use_flip'], self.opt['use_rot'])
+            img_LQs = rlt[0:-1]
+            img_GT = rlt[-1]
+
+        # stack LQ images to NHWC, N is the frame number
+        img_LQs = np.stack(img_LQs, axis=0)
+        # HWC => CHW
+        img_GT = torch.from_numpy(np.ascontiguousarray(
+                            np.transpose(img_GT, (2, 0, 1)))).float()
+        img_LQs = torch.from_numpy(np.ascontiguousarray(
+                            np.transpose(img_LQs, (0, 3, 1, 2)))).float()
         return {'LQs': img_LQs, 'GT': img_GT, 'key': key, 'scale': scale}
 
     def __len__(self):
